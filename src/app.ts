a13670e90e49611693e673908638d5e78ec22841
CodeHub <codehuub@gmail.com>
2023-04-21 17:59:41 +0100

    Revert "init passport config"
    This reverts commit a0c63cf47e388e124c9225c108f95147bde1012e.

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,7 @@ import 'module-alias/register'
 import express from 'express'
 import cors from 'cors'
 import httpStatus from 'http-status'
-import passport from 'passport'
-import session from 'express-session'
 import itensRouter from '@routers/itens-router'
-import authRouter from '@routers/auth'
 
 const PORT = process.env.PORT || 4000
 const HOSTNAME = process.env.HOSTNAME || 'http://localhost'
@@ -13,11 +10,6 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-//Passport configs
-app.use(session({ secret: "secret" }))
-app.use(passport.initialize())
-app.use(passport.session())
 
 app.get('/', (req, res) => {
     res.send('Bem-vindo! João.')
@@ -28,7 +20,6 @@ app.use(cors({
 }))
 
 app.use('/api', itensRouter)
-app.use('/', authRouter)
 
 app.use((req, res) => {
     res.status(httpStatus.NOT_FOUND)
