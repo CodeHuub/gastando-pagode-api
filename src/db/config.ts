f775839a3bd9267855a59d8bd91ed0eb6eab550d
CodeHuub <codehuub@gmail.com>
2023-04-29 20:16:34 +0100

    TODO for a new test

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,62 +1,66 @@
-import { config } from "dotenv"
-config({ path: `.env.${process.env.NODE_ENV}` })
+import { config } from "dotenv";
+config({ path: `.env.${process.env.NODE_ENV}` });
 
-import diff from 'microdiff'
-import { Dialect, Model, Sequelize } from 'sequelize'
-import { SequelizeHooks } from 'sequelize/types/hooks'
+import diff from "microdiff";
 
-import localCache from '../lib/local-cache'
+import { Dialect, Model, Sequelize } from "sequelize";
+import { SequelizeHooks } from "sequelize/types/hooks";
 
-const isTest = process.env.NODE_ENV === 'test'
+import localCache from "../lib/local-cache";
 
-const dbName = isTest ? process.env.TEST_DB_NAME as string : process.env.DB_NAME as string
-const dbUser = process.env.DB_USER as string
-const dbHost = process.env.DB_HOST
-const dbDriver = process.env.DB_DRIVER as Dialect
-const dbPassword = process.env.DB_PASSWORD
+const isTest = process.env.NODE_ENV === "test";
+
+const dbName = isTest
+  ? (process.env.TEST_DB_NAME as string)
+  : (process.env.DB_NAME as string);
+const dbUser = process.env.DB_USER as string;
+const dbHost = process.env.DB_HOST;
+const dbDriver = process.env.DB_DRIVER as Dialect;
+const dbPassword = process.env.DB_PASSWORD;
 
 const hooks: Partial<SequelizeHooks<Model<any, any>, any, any>> = {
-    afterUpdate: (instance: Model<any, any>) => {
-        const cacheKey = `${instance.constructor.name.toLowerCase()}s`
+  afterUpdate: (instance: Model<any, any>) => {
+    const cacheKey = `${instance.constructor.name.toLowerCase()}s`;
 
-        const currentData = instance.get({ plain: true })
+    const currentData = instance.get({ plain: true });
 
-        if (!localCache.hasKey(cacheKey)) {
-            return
-        }
+    if (!localCache.hasKey(cacheKey)) {
+      return;
+    }
 
-        const listingData = localCache.get<any>(cacheKey) as any[]
-        const itemIndex = listingData.findIndex((it) => it.id === instance.getDataValue('id'))
-        const oldItemData = ~itemIndex ? listingData[itemIndex] : {}
+    const listingData = localCache.get<any>(cacheKey) as any[];
+    const itemIndex = listingData.findIndex(
+      (it) => it.id === instance.getDataValue("id")
+    );
+    const oldItemData = ~itemIndex ? listingData[itemIndex] : {};
 
-        const instanceDiff = diff(oldItemData, currentData)
+    const instanceDiff = diff(oldItemData, currentData);
 
-        if (instanceDiff.length > 0) {
-            listingData[itemIndex] = currentData
-            localCache.set(cacheKey, listingData)
-        }
-    },
-    afterCreate: (instance: Model<any, any>) => {
-        const cacheKey = `${instance.constructor.name.toLowerCase()}s`
-        const currentData = instance.get({ plain: true })
+    if (instanceDiff.length > 0) {
+      listingData[itemIndex] = currentData;
+      localCache.set(cacheKey, listingData);
+    }
+  },
+  afterCreate: (instance: Model<any, any>) => {
+    const cacheKey = `${instance.constructor.name.toLowerCase()}s`;
+    const currentData = instance.get({ plain: true });
 
-        if (!localCache.hasKey(cacheKey)) {
-            return
-        }
+    if (!localCache.hasKey(cacheKey)) {
+      return;
+    }
 
-        const listingData = localCache.get<any>(cacheKey) as any[]
-        listingData.push(currentData)
+    const listingData = localCache.get<any>(cacheKey) as any[];
+    listingData.push(currentData);
 
-        localCache.set(cacheKey, listingData)
-    },
-}
-
+    localCache.set(cacheKey, listingData);
+  },
+};
 
 const sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
-    host: dbHost,
-    dialect: dbDriver,
-    logging: false,
-    define: { hooks }
-})
+  host: dbHost,
+  dialect: dbDriver,
+  logging: false,
+  define: { hooks },
+});
 
-export default sequelizeConnection
+export default sequelizeConnection;
--- tests/routes/user.routes.test.ts
+++ tests/routes/user.routes.test.ts
@@ -6,88 +6,115 @@ import { Response } from "supertest";
 import { getServer } from "@root/app";
 
 const dbTeardown = async () => {
-    await User.sequelize?.query("SET FOREIGN_KEY_CHECKS = 0")
-    await User.destroy({ cascade: true, truncate: true, force: true });
-    await User.sequelize?.query("SET FOREIGN_KEY_CHECKS = 1")
-}
+  await User.sequelize?.query("SET FOREIGN_KEY_CHECKS = 0");
+  await User.destroy({ cascade: true, truncate: true, force: true });
+  await User.sequelize?.query("SET FOREIGN_KEY_CHECKS = 1");
+};
 
-afterAll(done => {
-    getServer().closeAllConnections()
-    getServer().close()
-    done();
-})
+afterAll((done) => {
+  getServer().closeAllConnections();
+  getServer().close();
+  done();
+});
 
 describe("User resources", () => {
-    const URL = '/api/v1/users'
+  const URL = "/api/v1/users";
 
-    let tenantId: string
-    let user: IUserOutput
+  let tenantId: string;
+  let user: IUserOutput;
 
+  beforeAll(async () => {
+    [user] = await Promise.all([
+      User.create({
+        tenantId: crypto.randomUUID(),
+        name: "Joao",
+        email: "joao@gmail.com",
+        password: "password",
+      }),
+      User.create({
+        tenantId: crypto.randomUUID(),
+        name: "Emanuel",
+        email: "joao@gmail.com",
+        password: "password",
+      }),
+    ]);
+    ({ tenantId } = user);
+  });
 
-    beforeAll(async () => {
-        [user] = await Promise.all([
-            User.create({ tenantId: crypto.randomUUID(), name: 'Joao', email: 'joao@gmail.com', password: 'password' }),
-            User.create({ tenantId: crypto.randomUUID(), name: 'Emanuel', email: 'joao@gmail.com', password: 'password' }),
-        ]); ({ tenantId } = user)
-    })
+  afterAll(async () => {
+    await dbTeardown();
+  });
 
-    afterAll(async () => {
-        await dbTeardown()
-    })
+  describe("Get all users", () => {
+    it("should validate if the response is an array", async () => {
+      const response = await request.get(URL).expect(OK);
 
-    describe("Get all users", () => {
-        it("should validate if the response is an array", async () => {
-            const response = await request.get(URL).expect(OK)
-
-            expect(Array.isArray(response.body)).toBe(true);
-        });
-
-        it("should validate if the response is an array of IUser interface", async () => {
-            const response = await request.get(URL).expect(OK)
-            const allUser = response.body;
-            expect(allUser.length > 0).toEqual(true);
-            const user = allUser[0];
-
-            expect(user).toHaveProperty("name");
-            expect(user).toHaveProperty("email");
-            expect(user).toHaveProperty("tenantId");
-            expect(user).toHaveProperty("createdAt");
-        });
+      expect(Array.isArray(response.body)).toBe(true);
     });
 
-    describe("Get User by TenantId", () => {
-        // Verificar se retorna o user do tenantId passado
-        // Se o tenant Id for invalido, retornar message usuario n existe
-        // Verificar se o retorno é do tipo User
+    it("should validate if the response is an array of IUser interface", async () => {
+      const response = await request.get(URL).expect(OK);
+      const allUser = response.body;
+      expect(allUser.length > 0).toEqual(true);
+      const user = allUser[0];
 
-        it("should validate the response is a valid User object", async () => {
-            const response = await request.get(`${URL}/${tenantId}`).expect(OK)
-            const body = response.body;
+      expect(user).toHaveProperty("name");
+      expect(user).toHaveProperty("email");
+      expect(user).toHaveProperty("tenantId");
+      expect(user).toHaveProperty("createdAt");
+    });
+  });
 
-            expect(body).toHaveProperty("name");
-            expect(body).toHaveProperty("email");
-            expect(body).toHaveProperty("tenantId");
-            expect(body).toHaveProperty("createdAt");
+  describe("Get User by TenantId", () => {
+    // Verificar se retorna o user do tenantId passado
+    // Se o tenant Id for invalido, retornar message usuario n existe
+    // Verificar se o retorno é do tipo User
 
-            expect(body.tenantId).toEqual(tenantId)
-            expect(body.name).toEqual(user.name)
-            expect(body.email).toEqual(user.email)
-        });
+    it("should validate the response is a valid User object", async () => {
+      const response = await request.get(`${URL}/${tenantId}`).expect(OK);
+      const body = response.body;
 
-        it("should validate the Error response when the tenantId is invalid", async () => {
-            const response = await request.get(`${URL}/${tenantId}1`).expect(FORBIDDEN)
-            const error = response.body;
+      expect(body).toHaveProperty("name");
+      expect(body).toHaveProperty("email");
+      expect(body).toHaveProperty("tenantId");
+      expect(body).toHaveProperty("createdAt");
 
-            expect(error).toEqual({ message: 'User not found!' })
-        });
-    })
-    describe("Update User", () => {
+      expect(body.tenantId).toEqual(tenantId);
+      expect(body.name).toEqual(user.name);
+      expect(body.email).toEqual(user.email);
+    });
 
-    })
-    describe("Delete User", () => {
+    it("should validate the Error response when the tenantId is invalid", async () => {
+      const response = await request
+        .get(`${URL}/${tenantId}1`)
+        .expect(FORBIDDEN);
+      const error = response.body;
 
-    })
-    describe("Create User", () => {
-
-    })
+      expect(error).toEqual({ message: "User not found!" });
+    });
+  });
+  describe("Update User", () => {
+    /*
+            - Atualizar dados do User
+            - Verificar se os dados passados estão corretos
+            - Mensagem de erro caso algum preenchimento inválido
+            - Deve retornar os dados atualizados ao User
+            - 
+        */
+  });
+  describe("Delete User", () => {
+    /*
+            - Deletar User
+            - Deve retornar uma confirmação ao User
+            - Verificar se os dados passados estão corretos
+            - 
+        */
+  });
+  describe("Create User", () => {
+    /*
+            - Deve Criar um User
+            - Deve retornar msg de concluido
+            - Deve verificar os dados passados pelo User
+        */
+  });
 });
